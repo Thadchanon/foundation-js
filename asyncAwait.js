59cf29d87d9a06242f1850d0cf3013ee429dd103
Thadchanon <thadchanonjirarat@gmail.com>
2023-09-22 17:52:32 +0700

    add asyncAwait.js and promise.js

---
+++ asyncAwait.js
@@ -0,0 +1,60 @@
+/* const fetchData = () => Promise.resolve("Data is found");
+
+const getData = async () => {
+  try {
+    const res = await fetchData();
+
+    console.log("yo");
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("done");
+  }
+};
+
+getData();
+
+const fetchData = () => Promise.resolve("data"); */
+
+const fetchData1 = () => Promise.resolve("data1");
+const fetchData2 = () => Promise.resolve("data2");
+const fetchData3 = () => Promise.resolve("data3");
+
+// fetchData1()
+//   .then((res) => {
+//     console.log(res)
+//     fetchData2()
+//       .then((res) => {
+//         console.log(res)
+//         fetchData3()
+//           .then((res) => {
+//             console.log(res)
+//           })
+//           .catch((err) => {
+//             console.log(err)
+//           })
+//       })
+//       .catch((err) => {
+//         console.log(err)
+//       })
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+
+const getMultipleData = async () => {
+  try {
+    const result = await fetchData1();
+    const result2 = await fetchData2();
+    const result3 = await fetchData3();
+
+    console.log(result);
+    console.log(result2);
+    console.log(result3);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getMultipleData();
